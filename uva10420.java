d673cf73f1d07107ca416a8f944926a7854836bd
WAETW <37590423+WAETW@users.noreply.github.com>
2018-09-20 17:59:44 +0800

    Add files via upload

---
+++ uva10420.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class uva10420
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int count=sc.nextInt();
+        String input;
+        String country[] = new String[2000];
+        if(count>2000)
+            break;
+        for(int i = 0; i<=count;i++)
+        {
+            input = sc.nextLine();
+        }
+        
+
+        
+    }
+}
